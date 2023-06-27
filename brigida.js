1ce5113e22e399801f1d40b6c0960841980f5f7e
asseug <asseug23@gmail.com>
2023-06-27 16:09:56 -0300

    yig

---
+++ brigida.js
@@ -0,0 +1,10 @@
+document.getElementById("enviar").addEventListener("click", function(event) {
+    event.preventDefaut(); 
+
+var nome = document.getElementByld("nome").value;
+var email = document.getElementByld("email").value;
+
+console.log("Nome:", nome);
+console.log("Email:", email);
+
+});
