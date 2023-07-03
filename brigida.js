359176ed4bc7785acc96d14ae6e84968ea9468b0
asseug <asseug23@gmail.com>
2023-07-03 16:28:37 -0300

    eee

--- brigida.js
+++ brigida.js
@@ -7,4 +7,19 @@ var email = document.getElementByld("email").value;
 console.log("Nome:", nome);
 console.log("Email:", email);
 
-});
+
+fetch("https://formspree.io/f/form_id",{
+methody:"Post",
+headers:{
+"Acept":"aplication/json"
+},
+body:formData
+})
+.then(function(response){
+if(response.ok){
+
+alert("Formulário enviado com sucesso!");
+} else {
+alert(Ocorreu um erro ao enviar o formulário.Por favor tente novamente mais tarde!)}
+.catch(function(error;))
+alert("Ocorreu um erro ao enviar o formulário.Por favor, tente novamente mais tarde!")
