44146adbd99189ffe0fb268a2609da5b432ede46
asseug <asseug23@gmail.com>
2023-07-05 16:45:31 -0300

    projetofinal

--- brigida.js
+++ brigida.js
@@ -1,25 +1,36 @@
 document.getElementById("enviar").addEventListener("click", function(event) {
     event.preventDefaut(); 
 
-var nome = document.getElementByld("nome").value;
-var email = document.getElementByld("email").value;
+let nome = document.getElementByld("nome").value;
+let email = document.getElementByld("email").value;
 
 console.log("Nome:", nome);
 console.log("Email:", email);
 
+let FormData = new FormData();
+formData.append("email",email);
 
-fetch("https://formspree.io/f/form_id",{
-methody:"Post",
+
+
+
+fetch("https://formspree.io/f/form_id", {
+method:"post",
 headers:{
-"Acept":"aplication/json"
+    "Acept":"aplication/json"
 },
-body:formData
+  body:formData
 })
-.then(function(response){
-if(response.ok){
 
-alert("Formulário enviado com sucesso!");
+.then(function(response) { 
+    if(response.ok){
+        alert("Formulário enviado com sucesso!");
 } else {
-alert(Ocorreu um erro ao enviar o formulário.Por favor tente novamente mais tarde!)}
-.catch(function(error;))
-alert("Ocorreu um erro ao enviar o formulário.Por favor, tente novamente mais tarde!")
+    alert("Ocorreu um erro ao enviar o formulário.Por favor tente novamente mais tarde!")
+}
+});
+
+.catch(function(error) {
+        alert("Ocorreu um erro ao enviar o formulário.Por favor, tente novamente mais tarde!")
+});
+
+});
